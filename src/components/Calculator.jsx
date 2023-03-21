e54f235a933dbddd5ab6c99da67e549d0b4bc617
shayan1234554321 <kshayan091@gmail.com>
2023-03-21 22:26:15 +0500

    Design Calculator

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Calculator() {
+  return (
+    <div className='calContainer' >
+        <div className="display">0</div>
+        <form action="" className="buttons">
+            <button>AC</button>
+            <button>+/-</button>
+            <button>%</button>
+            <button>÷</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>x</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>-</button>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>+</button>
+            <button>0</button>
+            <button>.</button>
+            <button>=</button>
+        </form>
+    </div>
+  )
+}

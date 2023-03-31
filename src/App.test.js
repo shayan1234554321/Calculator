2e91e85d81c3d50fb068aefd53f78b0d1b3d4315
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-03-31 12:32:52 +0300

    Fix the operator test

--- src/App.test.js
+++ src/App.test.js
@@ -13,15 +13,15 @@ describe('Operate Tests', () => {
     expect(result).toBe('9');
   });
   it('4 - 5 equals -1', () => {
-    const result = operate(4, 5, '+');
+    const result = operate(4, 5, '-');
     expect(result).toBe('-1');
   });
   it('4 x 5 equals 20', () => {
-    const result = operate(4, 5, '+');
+    const result = operate(4, 5, 'x');
     expect(result).toBe('20');
   });
   it('4 ÷ 5 equals 0.8', () => {
-    const result = operate(4, 5, '+');
+    const result = operate(4, 5, '÷');
     expect(result).toBe('0.8');
   });
   it('4 % 5 equals 4', () => {
